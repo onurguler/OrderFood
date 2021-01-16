a9cf0e31330e1fcbfd20c5349b9828fa8e6f254b
Onur O. Güler <guleronurosman@gmail.com>
2021-01-17 02:29:49 +0300

    Admin product list

--- OrderFood.Application/Managers/ProductManager.cs
+++ OrderFood.Application/Managers/ProductManager.cs
@@ -37,5 +37,25 @@ namespace OrderFood.Application.Managers
 
             return products;
         }
+
+        public async Task<List<ProductDto>> SearchProducts(string searchString)
+        {
+            var query = _productRepository.GetAll().Include(product => product.ProductCategories).ThenInclude(productCategory => productCategory.Category).Select(product => new ProductDto()
+            {
+                Id = product.Id,
+                Title = product.Title,
+                Subtitle = product.Subtitle,
+                Description = product.Description,
+                ImageUrl = product.ImageUrl,
+                Price = product.Price,
+                Categories = product.ProductCategories.Select(productCategory => new CategoryDto()
+                {
+                    Id = productCategory.CategoryId,
+                    Title = productCategory.Category.Title
+                }).ToList()
+            }).OrderBy(product => product.Title).Where(product => product.Title.ToLower().Contains(searchString.ToLower()) || product.Subtitle.ToLower().Contains(searchString.ToLower()) || product.Description.ToLower().Contains(searchString.ToLower()));
+
+            return await query.ToListAsync();
+        }
     }
 }
---
+++ OrderFood.Web/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OrderFood.Domain.Dto;
+using OrderFood.Web.Controllers;
+using OrderFood.Web.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OrderFood.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ProductsController : BaseController
+    {
+        public ProductsController(WebBaseManager webBaseManager) : base(webBaseManager)
+        {
+                
+        }
+
+        public async Task<IActionResult> Index(string searchString)
+        {
+            List<ProductDto> products = null;
+
+            if (string.IsNullOrEmpty(searchString))
+            {
+                products = await WebBaseManager.ApplicationBaseManager.ProductManager.GetProductList();
+            }
+            else
+            {
+                products = await WebBaseManager.ApplicationBaseManager.ProductManager.SearchProducts(searchString);
+                ViewBag.SearchString = searchString;
+            }
+
+            return View(products);
+        }
+    }
+}
